9ebddcdfb5f07f07fd08b3b3376fd9d78fff6451
yamao2253 <yamao2253@gmail.com>
2014-03-08 18:51:37 +0900

    4.内包表記でfor+filterっぽく記述と5.集合型のintersection関数を使うを追加
    その他、小修正もしました

--- cmp_list.py
+++ cmp_list.py
@@ -24,9 +24,17 @@ def cmp_for_filter(src_list, tag_list):
 def cmp_set_list(src_list, tag_list):
     src_set = set(src_list)
     tag_set = set(tag_list)
-    matched_list = list(src_set & tag_set)
+    return list(src_set & tag_set)
 
-    return matched_list
+# 4. 内包表記で、for+filter っぽく記述
+def cmp_list_comprehension(src_list, tag_list):
+    return [tag for tag in tag_list if tag in src_list] 
+
+# 5. 集合型のintersection関数を使う
+def cmp_set_intersection(src_list, tag_list):
+    src_set = set(src_list)
+    tag_set = set(tag_list)
+    return list(src_set.intersection(tag_set))
 
 
 src_list=['taniguchi', 'matsushita', 'koyama', 'asama', 
@@ -37,20 +45,33 @@ argvs=sys.argv
 argc=len(argvs)
 tag_list=argvs[1:]
 
+
 print "# 1. それぞれのリストをfor文で回して要素を取り出し比較する"
-matched_list=cmp_for_for(src_list, tag_list)
+matched_list = cmp_for_for(src_list, tag_list)
 print matched_list , "\n"
 if len(matched_list) == 0:
     print "No match!"
 
 print "# 2. 1つのリストはfor文、もう1つはfilter関数で要素を取り出して比較する"
-matched_list=cmp_for_filter(src_list, tag_list)
+matched_list = cmp_for_filter(src_list, tag_list)
 print matched_list , "\n"
 if len(matched_list) == 0:
     print "No match!"
 
 print "# 3. リストを集合型(set)に変換して論理積をとる。その結果をリストに変換する"
-matched_list=cmp_set_list(src_list, tag_list)
+matched_list = cmp_set_list(src_list, tag_list)
+print matched_list , "\n"
+if len(matched_list) == 0:
+    print "No match!"
+
+print "# 4. 内包表記で、for+filter っぽく記述"
+matched_list = cmp_list_comprehension(src_list, tag_list)
+print matched_list , "\n"
+if len(matched_list) == 0:
+    print "No match!"
+
+print "# 5. 集合型のintersection関数を使う"
+matched_list = cmp_set_intersection(src_list, tag_list)
 print matched_list , "\n"
 if len(matched_list) == 0:
     print "No match!"
